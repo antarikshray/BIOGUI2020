f0e9025f736538a8b442cb7eb101bf4798bbd694
Pawan Wadhwani <pawanw17@gmail.com>
2019-12-30 05:34:46 +0530

    rosbridge port change json entry

--- js/comm.js
+++ js/comm.js
@@ -15,7 +15,7 @@ var writer = csvWriter({
 
 var setupServer = function () {
   var ros = new ROSLIB.Ros({
-    url : 'ws://localhost:9090'
+    url : 'ws://localhost:9091'
   });
 
   ros.on('connection', function(){
